edb0a28f8f8f538c370ff6d95a1ee74954cf59d9
Salakar <mike.diarmid@gmail.com>
2018-06-08 19:15:42 +0100

    start of tests suite

---
+++ src/__tests__/connectors/standalone.test.js
@@ -0,0 +1,42 @@
+/* eslint-env jest */
+
+const StandaloneConnector = require('./../../Connector/Standalone');
+const Commander = require('./../../Commander/Commander');
+
+const DEFAULT_OPTIONS = { host: '127.0.0.1', port: 6379 };
+
+describe('StandaloneConnector', () => {
+  test('creates commander instance if none specified in options', () => {
+    const redis = new StandaloneConnector(DEFAULT_OPTIONS);
+    expect(redis.commander).toBeInstanceOf(Commander);
+  });
+
+  test('uses existing commander if provided in options', () => {
+    const commander = new Commander();
+    const redis = new StandaloneConnector({ ...DEFAULT_OPTIONS, commander });
+    expect(redis.commander).toBe(commander);
+  });
+
+  test('is an event emitter', () => {
+    const redis = new StandaloneConnector({ ...DEFAULT_OPTIONS });
+    expect(typeof redis.on).toBe('function');
+    expect(typeof redis.emit).toBe('function');
+    expect(typeof redis.once).toBe('function');
+  });
+
+  test.skip('Queues commands prior to connecting', () => {
+    const redis = new StandaloneConnector(DEFAULT_OPTIONS);
+    redis.commander.set('moo', 'bar');
+    redis.commander.set('moo', 'bar');
+    redis.commander.get('moo');
+    redis.commander.info();
+    redis.commander.ping();
+    redis.commander.ping();
+    redis.commander.ping();
+    redis.commander.ping();
+    redis.commander.set('moo', 'bar');
+    redis.commander.set('moo', 'bar');
+    redis.commander.set('moo', 'bar');
+    redis.connect();
+  });
+});
