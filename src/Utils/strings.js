e513065023a1be6c043838341ba819766a1cf955
Salakar <mike.diarmid@gmail.com>
2018-06-09 00:35:57 +0100

    start of Utils/strings - all errors and strings to be stored in one location

---
+++ src/Utils/strings.js
@@ -0,0 +1,9 @@
+module.exports = {
+  STATUS_CONNECTED: 'connected',
+  STATUS_CONNECTING: 'connecting',
+  STATUS_DISCONNECTED: 'disconnected',
+  STATUS_DISCONNECTING: 'disconnecting',
+
+  ERROR_ALREADY_CONNECTED:
+    'ABORT: Connector is already connecting or connected!',
+};
